f0777ebba3d333515a92675a72f0857a02e02bcd
Hariharan <hariharan9572@gmail.com>
2026-01-04 21:20:01 +0530

    ManyToMany Mapping

--- src/main/java/com/hariharan/Main.java
+++ src/main/java/com/hariharan/Main.java
@@ -23,14 +23,34 @@ public class Main {
         l2.setModel("XPS");
         l2.setRam(32);
 
+        Laptop l3 = new Laptop();
+        l3.setLid(3);
+        l3.setBrand("Apple");
+        l3.setModel("Macbook air");
+        l3.setRam(8);
+
         Alien a1 = new Alien();
         a1.setAid(101);
         a1.setAname("Hari");
         a1.setTech("Java");
-        a1.setLaptops(Arrays.asList(l1,l2));
 
-        l1.setAlien(a1);
-        l2.setAlien(a1);
+        Alien a2 = new Alien();
+        a2.setAid(102);
+        a2.setAname("Divya");
+        a2.setTech("Python");
+
+        Alien a3 = new Alien();
+        a3.setAid(103);
+        a3.setAname("Viji");
+        a3.setTech("AI");
+
+        a1.setLaptops(Arrays.asList(l1,l2));
+        a2.setLaptops(Arrays.asList(l2,l3));
+        a3.setLaptops(Arrays.asList(l1));
+
+        l1.setAliens(Arrays.asList(a1,a3));
+        l2.setAliens(Arrays.asList(a1,a2));
+        l3.setAliens(Arrays.asList(a2));
 
         SessionFactory sf = new Configuration()
                 .addAnnotatedClass(com.hariharan.Alien.class)
@@ -44,12 +64,16 @@ public class Main {
 
         session.persist(l1);
         session.persist(l2);
+        session.persist(l3);
+
         session.persist(a1);
+        session.persist(a2);
+        session.persist(a3);
 
         transaction.commit();
 
-        Alien a2 = session.get(Alien.class, 101);
-        System.out.println(a2);
+        Alien a5 = session.get(Alien.class, 102);
+        System.out.println(a5);
 
         session.close();
         sf.close();
