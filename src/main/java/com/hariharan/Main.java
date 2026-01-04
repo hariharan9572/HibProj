78e6434b074ab5d677fbb42e7dee4701deddfa87
Hariharan <hariharan9572@gmail.com>
2026-01-04 20:22:45 +0530

    Changing Table and Column names

--- src/main/java/com/hariharan/Main.java
+++ src/main/java/com/hariharan/Main.java
@@ -8,44 +8,26 @@ import org.hibernate.cfg.Configuration;
 public class Main {
     public static void main(String[] args) {
 
-        Student s1 = new Student();
-        /*s1.setsName("Navin");
-        s1.setRollNo(109);
-        s1.setsAge(30);*/
+        Alien a1 = new Alien();
+        a1.setAid(101);
+        a1.setAname("Hari");
+        a1.setTech("Java");
 
-        //Student s2 = null;
-
-        /*Configuration cfg = new Configuration();
-        cfg.addAnnotatedClass(com.hariharan.Student.class);
-        cfg.configure();*/
-
-        //SessionFactory sf = cfg.buildSessionFactory();
         SessionFactory sf = new Configuration()
-                .addAnnotatedClass(com.hariharan.Student.class)
+                .addAnnotatedClass(com.hariharan.Alien.class)
                 .configure()
                 .buildSessionFactory();
 
         Session session = sf.openSession();
 
-        s1 = session.get(Student.class,109);
-
         Transaction transaction = session.beginTransaction();
 
-        //session.persist(s1);
-        //session.merge(s1);
-        session.remove(s1);
+        session.persist(a1);
 
         transaction.commit();
 
-        //s2 = session.get(Student.class, 102); @depreciated
-        //s2 = session.load(Student.class, 102);
-        //s2 = session.find(Student.class, 102);
-
         session.close();
         sf.close();
-
-        System.out.println(s1);
-        //System.out.println(s2);
 
     }
 }
