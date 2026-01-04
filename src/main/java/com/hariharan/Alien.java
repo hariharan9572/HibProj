f4b8c1ac993141716594af25a9d42e6beb64345e
Hariharan <hariharan9572@gmail.com>
2026-01-04 21:02:13 +0530

    OneToMany and ManyToOne

--- src/main/java/com/hariharan/Alien.java
+++ src/main/java/com/hariharan/Alien.java
@@ -2,6 +2,8 @@ package com.hariharan;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 public class Alien {
 
@@ -9,8 +11,8 @@ public class Alien {
     private int aid;
     private String aname;
     private String tech;
-    @OneToOne
-    private Laptop laptop;
+    @OneToMany(mappedBy = "alien")
+    private List<Laptop> laptops;
 
 
     public int getAid() {
@@ -37,12 +39,12 @@ public class Alien {
         this.tech = tech;
     }
 
-    public Laptop getLaptop() {
-        return laptop;
+    public List<Laptop> getLaptops() {
+        return laptops;
     }
 
-    public void setLaptop(Laptop laptop) {
-        this.laptop = laptop;
+    public void setLaptops(List<Laptop> laptops) {
+        this.laptops = laptops;
     }
 
     @Override
@@ -51,7 +53,7 @@ public class Alien {
                 "aid=" + aid +
                 ", aname='" + aname + '\'' +
                 ", tech='" + tech + '\'' +
-                ", laptop=" + laptop +
+                ", laptops=" + laptops +
                 '}';
     }
 }
--- src/main/java/com/hariharan/Laptop.java
+++ src/main/java/com/hariharan/Laptop.java
@@ -1,8 +1,6 @@
 package com.hariharan;
 
-import jakarta.persistence.Embeddable;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Laptop {
@@ -12,6 +10,8 @@ public class Laptop {
     private String brand;
     private String model;
     private int ram;
+    @ManyToOne
+    private Alien alien;
 
     public int getLid() {
         return lid;
@@ -43,6 +43,14 @@ public class Laptop {
 
     public void setRam(int ram) {
         this.ram = ram;
+    }
+
+    public Alien getAlien() {
+        return alien;
+    }
+
+    public void setAlien(Alien alien) {
+        this.alien = alien;
     }
 
     @Override
